09046cb7eb6420e75a5f0b917e68826aa43364ae
Yikang Li (Ethan) <ethanli2003@outlook.com>
2025-02-21 03:57:24 +0800

    Update Timestamp.java

--- src/main/java/org/example/utils/Timestamp.java
+++ src/main/java/org/example/utils/Timestamp.java
@@ -1,4 +1,22 @@
 package org.example.utils;
 
-public class Timestamp {
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public record Timestamp(int year, int month, int day, int hour, int minute, int second) {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    public Timestamp(String timestamp) {
+        this(LocalDateTime.parse(timestamp, FORMATTER));
+    }
+
+    public Timestamp(LocalDateTime dateTime) {
+        this(dateTime.getYear(), dateTime.getMonthValue(), dateTime.getDayOfMonth(),
+                dateTime.getHour(), dateTime.getMinute(), dateTime.getSecond());
+    }
+
+    @Override
+    public String toString() {
+        return LocalDateTime.of(year, month, day, hour, minute, second).format(FORMATTER);
+    }
 }
